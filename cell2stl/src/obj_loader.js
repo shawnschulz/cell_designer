9a8e15372032acea0edec293bb6285ca964bf393
Shawn Schulz <shawnschulz@berkeley.edu>
2024-10-20 21:00:36 -0700

    Reorganize files, git rid of large data files

--- cell2stl/src/obj_loader.js
+++ cell2stl/src/obj_loader.js
@@ -8,12 +8,14 @@ function parseOBJ(text) {
   const objPositions = [[0, 0, 0]];
   const objTexcoords = [[0, 0]];
   const objNormals = [[0, 0, 0]];
+  const objColors = [[0, 0, 0]];
 
   // same order as `f` indices
   const objVertexData = [
     objPositions,
     objTexcoords,
     objNormals,
+    objColors,
   ];
 
   // same order as `f` indices
@@ -21,7 +23,17 @@ function parseOBJ(text) {
     [],   // positions
     [],   // texcoords
     [],   // normals
+    [],   // colors
   ];
+
+  const materialLibs = [];
+  const geometries = [];
+  let geometry;
+  let groups = ['default'];
+  let material = 'default';
+  let object = 'default';
+
+  const noop = () => {};
 
   function newGeometry() {
     // If there is an existing geometry and it's
@@ -29,7 +41,33 @@ function parseOBJ(text) {
     if (geometry && geometry.data.position.length) {
       geometry = undefined;
     }
-    setGeometry();
+  }
+
+  function setGeometry() {
+    if (!geometry) {
+      const position = [];
+      const texcoord = [];
+      const normal = [];
+      const color = [];
+      webglVertexData = [
+        position,
+        texcoord,
+        normal,
+        color,
+      ];
+      geometry = {
+        object,
+        groups,
+        material,
+        data: {
+          position,
+          texcoord,
+          normal,
+          color,
+        },
+      };
+      geometries.push(geometry);
+    }
   }
 
   function addVertex(vert) {
@@ -41,12 +79,23 @@ function parseOBJ(text) {
       const objIndex = parseInt(objIndexStr);
       const index = objIndex + (objIndex >= 0 ? 0 : objVertexData[i].length);
       webglVertexData[i].push(...objVertexData[i][index]);
+      // if this is the position index (index 0) and we parsed
+      // vertex colors then copy the vertex colors to the webgl vertex color data
+      if (i === 0 && objColors.length > 1) {
+        geometry.data.color.push(...objColors[index]);
+      }
     });
   }
 
   const keywords = {
     v(parts) {
-      objPositions.push(parts.map(parseFloat));
+      // if there are more than 3 values here they are vertex colors
+      if (parts.length > 3) {
+        objPositions.push(parts.slice(0, 3).map(parseFloat));
+        objColors.push(parts.slice(3).map(parseFloat));
+      } else {
+        objPositions.push(parts.map(parseFloat));
+      }
     },
     vn(parts) {
       objNormals.push(parts.map(parseFloat));
@@ -56,12 +105,31 @@ function parseOBJ(text) {
       objTexcoords.push(parts.map(parseFloat));
     },
     f(parts) {
+      setGeometry();
       const numTriangles = parts.length - 2;
       for (let tri = 0; tri < numTriangles; ++tri) {
         addVertex(parts[0]);
         addVertex(parts[tri + 1]);
         addVertex(parts[tri + 2]);
       }
+    },
+    s: noop,    // smoothing group
+    mtllib(parts, unparsedArgs) {
+      // the spec says there can be multiple filenames here
+      // but many exist with spaces in a single filename
+      materialLibs.push(unparsedArgs);
+    },
+    usemtl(parts, unparsedArgs) {
+      material = unparsedArgs;
+      newGeometry();
+    },
+    g(parts) {
+      groups = parts;
+      newGeometry();
+    },
+    o(parts, unparsedArgs) {
+      object = unparsedArgs;
+      newGeometry();
     },
   };
 
@@ -86,83 +154,153 @@ function parseOBJ(text) {
     handler(parts, unparsedArgs);
   }
 
+  // remove any arrays that have no entries.
+  for (const geometry of geometries) {
+    geometry.data = Object.fromEntries(
+        Object.entries(geometry.data).filter(([, array]) => array.length > 0));
+  }
+
   return {
-    position: webglVertexData[0],
-    texcoord: webglVertexData[1],
-    normal: webglVertexData[2],
+    geometries,
+    materialLibs,
   };
 }
-
-function getIndices(array)
-{
-    let ret = []
-    for (let i = 0; i < array.length; i++){
-        ret.push(i)
-    } 
-    return ret
-}
-  function createBuffer(gl, data) {
-      // Because data is just named arrays like this
-      //
-          // {
-              //   position: [...],
-              //   texcoord: [...],
-              //   normal: [...],
-              // }
-      //
-          // and because those names match the attributes in our vertex
-      // shader we can pass it directly into `createBufferInfoFromArrays`
-      // from the article "less code more fun".
-
-          // create a buffer for each array by calling
-      // gl.createBuffer, gl.bindBuffer, gl.bufferData
-      //const bufferInfo = webglUtils.createBufferInfoFromArrays(gl, data);
-      const bin_buffer = gl.createBuffer()
-
-      const bin_normal = new Float32Array(data.normal)
-//      const tex_i = getIndices(data.normal)
-      gl.bindBuffer(gl.ARRAY_BUFFER, bin_buffer)
-      gl.bufferData(gl.ARRAY_BUFFER, bin_normal, gl.STATIC_DRAW)
-
-      const tex_buffer = gl.createBuffer()
-      const bin_texture = new Float32Array(data.texcoord)
-//      const tex_i = getIndices(data.texcoord)
-      gl.bindBuffer(gl.ARRAY_BUFFER, tex_buffer)
-      gl.bufferData(gl.ARRAY_BUFFER, bin_texture, gl.STATIC_DRAW)
-      // This could be gemini hallucinating, but i think we need to use typed
-      // arrays so its actually binary data
-      
-      const pos_buffer = gl.createBuffer()
-      const bin_positions = new Float32Array(data.position)
- //     const pos_i = getIndices(data.position)
-      gl.bindBuffer(gl.ARRAY_BUFFER, pos_buffer)
-      gl.bufferData(gl.ARRAY_BUFFER, bin_positions, gl.STATIC_DRAW)
-  }
 
 async function main() {
   // Get A WebGL context
   /** @type {HTMLCanvasElement} */
-    const canvas = document.getElementById("canvas");
-
+  const canvas = document.querySelector("#canvas");
   const gl = canvas.getContext("webgl");
   if (!gl) {
     return;
   }
 
+  const vs = `
+  attribute vec4 a_position;
+  attribute vec3 a_normal;
+  attribute vec4 a_color;
+
+  uniform mat4 u_projection;
+  uniform mat4 u_view;
+  uniform mat4 u_world;
+
+  varying vec3 v_normal;
+  varying vec4 v_color;
+
+  void main() {
+    gl_Position = u_projection * u_view * u_world * a_position;
+    v_normal = mat3(u_world) * a_normal;
+    v_color = a_color;
+  }
+  `;
+
+  const fs = `
+  precision mediump float;
+
+  varying vec3 v_normal;
+  varying vec4 v_color;
+
+  uniform vec4 u_diffuse;
+  uniform vec3 u_lightDirection;
+
+  void main () {
+    vec3 normal = normalize(v_normal);
+    float fakeLight = dot(u_lightDirection, normal) * .5 + .5;
+    vec4 diffuse = u_diffuse * v_color;
+    gl_FragColor = vec4(diffuse.rgb * fakeLight, diffuse.a);
+  }
+  `;
+
 
   // compiles and links the shaders, looks up attribute and uniform locations
- // const meshProgramInfo = webglUtils.createProgramInfo(gl, [vs, fs]);
+  const meshProgramInfo = webglUtils.createProgramInfo(gl, [vs, fs]);
+
   const response = await fetch('./data/cells/stromal_like.obj');  
   const text = await response.text();
-  const data = parseOBJ(text);
+  const obj = parseOBJ(text);
 
-  
-  createBuffer(gl, data);
+  const parts = obj.geometries.map(({data}) => {
+    // Because data is just named arrays like this
+    //
+    // {
+    //   position: [...],
+    //   texcoord: [...],
+    //   normal: [...],
+    // }
+    //
+    // and because those names match the attributes in our vertex
+    // shader we can pass it directly into `createBufferInfoFromArrays`
+    // from the article "less code more fun".
 
+    if (data.color) {
+      if (data.position.length === data.color.length) {
+        // it's 3. The our helper library assumes 4 so we need
+        // to tell it there are only 3.
+        data.color = { numComponents: 3, data: data.color };
+      }
+    } else {
+      // there are no vertex colors so just use constant white
+      data.color = { value: [1, 1, 1, 1] };
+    }
+
+    // create a buffer for each array by calling
+    // gl.createBuffer, gl.bindBuffer, gl.bufferData
+    const bufferInfo = webglUtils.createBufferInfoFromArrays(gl, data);
+    return {
+      material: {
+        u_diffuse: [1, 1, 1, 1],
+      },
+      bufferInfo,
+    };
+  });
+
+  function getExtents(positions) {
+    const min = positions.slice(0, 3);
+    const max = positions.slice(0, 3);
+    for (let i = 3; i < positions.length; i += 3) {
+      for (let j = 0; j < 3; ++j) {
+        const v = positions[i + j];
+        min[j] = Math.min(v, min[j]);
+        max[j] = Math.max(v, max[j]);
+      }
+    }
+    return {min, max};
+  }
+
+  function getGeometriesExtents(geometries) {
+    return geometries.reduce(({min, max}, {data}) => {
+      const minMax = getExtents(data.position);
+      return {
+        min: min.map((min, ndx) => Math.min(minMax.min[ndx], min)),
+        max: max.map((max, ndx) => Math.max(minMax.max[ndx], max)),
+      };
+    }, {
+      min: Array(3).fill(Number.POSITIVE_INFINITY),
+      max: Array(3).fill(Number.NEGATIVE_INFINITY),
+    });
+  }
+
+  const extents = getGeometriesExtents(obj.geometries);
+  const range = m4.subtractVectors(extents.max, extents.min);
+  // amount to move the object so its center is at the origin
+  const objOffset = m4.scaleVector(
+      m4.addVectors(
+        extents.min,
+        m4.scaleVector(range, 0.5)),
+      -1);
   const cameraTarget = [0, 0, 0];
-  const cameraPosition = [0, 0, 4];
-  const zNear = 0.1;
-  const zFar = 50;
+  // figure out how far away to move the camera so we can likely
+  // see the object.
+  const radius = m4.length(range) * 1.2;
+  const cameraPosition = m4.addVectors(cameraTarget, [
+    0,
+    0,
+    radius,
+  ]);
+  // Set zNear and zFar to something hopefully appropriate
+  // for the size of this object.
+  const zNear = radius / 100;
+  const zFar = radius * 3;
 
   function degToRad(deg) {
     return deg * Math.PI / 180;
@@ -170,28 +308,49 @@ async function main() {
 
   function render(time) {
     time *= 0.001;  // convert to seconds
-      // non-dynamic canvas size
-    gl.canvas.width = window.innerWidth;
-    gl.canvas.height = window.innerHeight;
+
+    webglUtils.resizeCanvasToDisplaySize(gl.canvas);
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
     gl.enable(gl.DEPTH_TEST);
-    gl.enable(gl.CULL_FACE);
 
-           // Setup the pointer to our attribute data (the triangles)
-      gl.enableVertexAttribArray(gl.positionLocation);
-      gl.vertexAttribPointer(gl.positionLocation, 3, gl.FLOAT, false, 0, 0);
+    const fieldOfViewRadians = degToRad(60);
+    const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+    const projection = m4.perspective(fieldOfViewRadians, aspect, zNear, zFar);
 
-      // lets just make them red for now
-      const red = new Float32Array([0.0, 0.0, 1.0, 1.0]);
-      // Setup the color uniform that will be shared across all triangles
-      gl.uniform4fv(gl.colorLocation, red);
+    const up = [0, 1, 0];
+    // Compute the camera's matrix using look at.
+    const camera = m4.lookAt(cameraPosition, cameraTarget, up);
 
-      // DEBUG
-      let test = gl.getParameter(gl.ARRAY_BUFFER_BINDING);
+    // Make a view matrix from the camera matrix.
+    const view = m4.inverse(camera);
 
-      // Draw the triangles to the screen
-      gl.drawArrays(gl.TRIANGLES, 0, 6);
+    const sharedUniforms = {
+      u_lightDirection: m4.normalize([-1, 3, 5]),
+      u_view: view,
+      u_projection: projection,
+    };
 
+    gl.useProgram(meshProgramInfo.program);
+
+    // calls gl.uniform
+    webglUtils.setUniforms(meshProgramInfo, sharedUniforms);
+
+    // compute the world matrix once since all parts
+    // are at the same space.
+    let u_world = m4.yRotation(time);
+    u_world = m4.translate(u_world, ...objOffset);
+
+    for (const {bufferInfo, material} of parts) {
+      // calls gl.bindBuffer, gl.enableVertexAttribArray, gl.vertexAttribPointer
+      webglUtils.setBuffersAndAttributes(gl, meshProgramInfo, bufferInfo);
+      // calls gl.uniform
+      webglUtils.setUniforms(meshProgramInfo, {
+        u_world,
+        u_diffuse: material.u_diffuse,
+      });
+      // calls gl.drawArrays or gl.drawElements
+      webglUtils.drawBufferInfo(gl, bufferInfo);
+    }
 
     requestAnimationFrame(render);
   }
