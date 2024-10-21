4dfdeeac85fae8acc8a55fb7c58c7c947d6105fb
Shawn Schulz <shawnschulz@berkeley.edu>
2024-10-20 22:50:05 -0700

    Add vite back in

--- cell2stl/src/linalg_test.js
+++ cell2stl/src/linalg_test.js
@@ -2,4 +2,4 @@ import * as la from './linalg.js';
 
 var test = la.convert_contiguous([1,1,1, 4,3,-1,3,5,3], [3,3]);
 var ident = la.identity(3, 3)
-var test2 = la.solve(test, ident)
+var test2 = la.inverse(test, ident)
