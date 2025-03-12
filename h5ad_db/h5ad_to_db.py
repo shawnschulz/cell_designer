6c01d9aeeef4284b859cc029451e3e17b9de5446
shawnschulz <shawnschulz@gberkeley.edu>
2025-03-11 22:49:20 -0700

    NEW PLAN: Just use vector databases and h5ad. I think we can actually classify nuclear receptor vs non receptor vs surface receptor vs exported using the vector embedding itself, albeit with a little elbow grease. Try both just using qdrant, using qdrant + other python libraries, and if that doesn't work you can try grabbing the embedding data in C++ and using it with cuVS

--- h5ad_db/h5ad_to_db.py
+++ h5ad_db/h5ad_to_db.py
@@ -1,16 +1,15 @@
-from AnnSQL import AnnSQL
-from AnnSQL.MakeDb import MakeDb
 import scanpy as sc
 import structlog
-import duckdb
-import lancedb
-import argparse
+from qdrant_client import QdrantClient
+from qdrant_client.models import Distance, VectorParams
 import time
+import argparse
 
 logger = structlog.get_logger(__name__)
+client = QdrantClient(host="localhost", port=6333)
 
 def setup_argparse():
-    parser = argparse.ArgumentParser(description='Generate or update database from h5ad files')
+    parser = argparse.ArgumentParser(description='Generate or update vector database fromr h5ad files')
     
     parser.add_argument('--create', 
                         type=str,  # Can change to int, float, etc.
@@ -24,37 +23,30 @@ def setup_argparse():
     
     return parser.parse_args()
 
-def create_duckdb(initial_h5ads, db_name="cell_database", db_path="db/"):
+# Idea here is to add hte gene sequences before adding to the vector database,
+# ideally this should make clustering a breeze and add search functionality
+# both for our receptor classificaiton task and for cell type classification
+# among other parts of the problem space
+def add_gene_sequences_to_h5ad(h5ad):
+    pass
+
+def create_vector_database(initial_h5ads, db_name="cell_database", vector_size=768):
     logger.info(f"Creating database {db_name} from the following paths: {initial_h5ads}")
     count = 0
+    client.create_collection(collection_name=db_name, vectors_config=VectorParams(size=vector_size, distance=Distance.DOT))
     
     for path in initial_h5ads:
         logger.info(f"Path processing: {path}")
-        if count == 0:
-            try:
-                adata = sc.read_h5ad(path)
-            except Exception as e:
-                logger.error(f"Reading path: {path} failed, trying next path")
-                logger.error(f"{e}")
-                continue
-            start_time = time.perf_counter()
-            MakeDb(adata=adata, db_name=db_name, db_path=db_path, make_buffer_file=True)
-            end_time = time.perf_counter()
-            logger.info(f"Database creation completed, took {(start_time - end_time):.4f} seconds")
-        else:
-            try:
-                adata = sc.read_h5ad(path)
-            except Exception as e:
-                logger.error(f"Reading path: {path} failed, trying next path")
-                logger.error(f"{e}")
-                continue
-            start_time = time.perf_counter()
-            new_asql = AnnSQL(adata=adata)
-            existing_asql = AnnSQL(db= db_path + db_name + ".asql")
-            existing_asql.query("INSERT INTO existing_asql * FROM new_asql")
-            end_time = time.perf_counter()
-            logger.info(f"Database update completed, took {(start_time - end_time):.4f} seconds")
-        count += 1
+        try:
+            adata = sc.read_h5ad(path)
+        except Exception as e:
+            logger.error(f"Reading path: {path} failed, trying next path")
+            logger.error(f"{e}")
+            continue
+        start_time = time.perf_counter()
+        ### INSERT CODE TO INSERT INTO VECTOR DATABASE HERE ###
+        end_time = time.perf_counter()
+        logger.info(f"Database creation completed, took {(start_time - end_time):.4f} seconds")
 
 # Not really convinced this will work depending on how in-memory is implemented
 # Might need to create a temporary on disk datbase and try using duck db bindings
